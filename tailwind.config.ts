2228241940a37d6fb203294b1f47456ef151d10f
NattawatC <64011478@kmitl.ac.th>
2024-02-15 10:24:09 +0700

    Delete unused files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      color:{
+        'primary': '#FFEEEF',
+        'secondary': '#B12753',
+        'text': '#482F33',
+        'custom-white': '#FFFFFF'
+      }
     },
   },
   plugins: [],
