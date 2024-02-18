001a25326b91f1330a8d04fae4c55400f57f5303
NattawatC <64011478@kmitl.ac.th>
2024-02-18 19:30:25 +0700

    Create Graphical data tab

--- src/pages/foodLog.tsx
+++ src/pages/foodLog.tsx
@@ -6,8 +6,10 @@ import { Navbar } from "@/components/Navbar"
 import { MainLayout } from "@/components/layout"
 import { Button } from "@/components/ui/button"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { BarChart } from "@tremor/react"
 import { NextPage } from "next"
 import { useRouter } from "next/router"
+import { useState } from "react"
 import { FaPlus } from "react-icons/fa"
 
 const props = [
@@ -37,8 +39,45 @@ const props = [
   },
 ]
 
+const data = [
+  {
+    date: "จัน",
+    carbs: 30,
+  },
+  {
+    date: "อังคาร",
+    carbs: 10,
+  },
+  {
+    date: "พุธ",
+    carbs: 20,
+  },
+  {
+    date: "พฤหัส",
+    carbs: 5,
+  },
+  {
+    date: "ศุกร์",
+    carbs: 3,
+  },
+  {
+    date: "เสาร์",
+    carbs: 9,
+  },
+  {
+    date: "อาทิต",
+    carbs: 2,
+  },
+]
+
+const dataFormatter = (number: number) =>
+  Intl.NumberFormat("us").format(number).toString()
+
+console.log(dataFormatter(1000000))
+
 const FoodLog: NextPage = () => {
   const router = useRouter()
+  const [showComparison, setShowComparison] = useState(false)
 
   const goToAddFood = () => {
     router.push("/addFood")
@@ -48,7 +87,7 @@ const FoodLog: NextPage = () => {
     <>
       <Navbar />
       <MainLayout className="flex flex-col gap-8 bg-primary ">
-        <Tabs defaultValue="บันทึกการบริโภค" className="flex flex-col w-auto gap-8">
+        <Tabs defaultValue="บันทึกการบริโภค" className="flex flex-col w-auto">
           <TabsList className="w-full text-xl">
             <TabsTrigger className="text-xl text-text" value="บันทึกการบริโภค">
               บันทึกการบริโภค
@@ -59,7 +98,9 @@ const FoodLog: NextPage = () => {
           </TabsList>
           <TabsContent value="บันทึกการบริโภค" className="flex flex-col gap-8 ">
             <div className="flex flex-col gap-4">
-              <h1 className="flex justify-center text-3xl">บันทึกการบริโภค</h1>
+              <h1 className="flex justify-center text-3xl pt-8">
+                บันทึกการบริโภค
+              </h1>
               <Gumpun />
             </div>
             <div className="flex flex-col gap-2">
@@ -98,7 +139,38 @@ const FoodLog: NextPage = () => {
             </div>
           </TabsContent>
           <TabsContent value="ประวัติการบริโภค">
-            Change your password here.
+            <Tabs
+              defaultValue="รายวัน"
+              className="flex flex-col w-auto gap-4 pt-8"
+            >
+              <TabsList className="w-full text-xl justify-between">
+                <TabsTrigger value="รายวัน">รายวัน</TabsTrigger>
+                <TabsTrigger value="รายสัปดาห์">รายสัปดาห์</TabsTrigger>
+                <TabsTrigger value="รายเดือน">รายเดือน</TabsTrigger>
+              </TabsList>
+              <TabsContent value="รายวัน">
+                <div className="flex flex-col gap-4">
+                  <div className="flex flex-col gap-1">
+                    <h3 className="flex text-lg font-medium text-text justify-center">
+                      ปริมาณการบริโภคคาร์บ ต่อวัน
+                    </h3>
+                    <hr className="bg-text opacity-30 rounded h-1" />
+                  </div>
+                  <div className="px-5 bg-white rounded-md ">
+                    <BarChart
+                      className="h-72 text-tremor-content-subtle borderRadius-tremor-default tremor-background-muted"
+                      data={data}
+                      index="date"
+                      categories={["carbs"]}
+                      colors={["#B12753"]}
+                      yAxisWidth={30}
+                    />
+                  </div>
+                </div>
+              </TabsContent>
+              <TabsContent value="รายสัปดาห์">รายสัปดาห์</TabsContent>
+              <TabsContent value="รายเดือน">รายเดือน</TabsContent>
+            </Tabs>
           </TabsContent>
         </Tabs>
       </MainLayout>
