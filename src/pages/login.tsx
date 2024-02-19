a5e2d0012d2369f827eb3d877642647783662f1f
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2024-02-19 16:56:28 +0700

    Login Page UI

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,17 @@
-export default function login() {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
+import LoginForm from "@/components/LoginForm"
+import { MainLayout } from "@/components/layout"
+import React from "react"
+import { FaBowlFood } from "react-icons/fa6"
+
+const login = () => {
+  return (
+    <MainLayout>
+      <div className="flex flex-col items-center justify-start min-h-screen bg-primary">
+        <FaBowlFood className="text-yellow-950 text-8xl mt-20 mb-20" />
+        <LoginForm />
+      </div>
+    </MainLayout>
+  )
 }
+
+export default login
---
+++ src/pages/signup.tsx
@@ -0,0 +1,5 @@
+const signup = () => {
+  return <div>Signup Page</div>
+}
+
+export default signup
