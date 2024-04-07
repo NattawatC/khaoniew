cf63f4c9a77e5e6af9c2c2bcb68cd9ce2d7bb0e0
DelighteningPigeon <89842170+fillpattana@users.noreply.github.com>
2024-04-08 00:26:59 +0700

    Patient Info Display (incomplete)
    1. lacks Staff's Desciption (ข้อควรระวัง)
    2. {thaiId} is still manually typed in

--- src/pages/personal.tsx
+++ src/pages/personal.tsx
@@ -1,12 +1,25 @@
 "use client"
 
-import { Navbar } from "@/components/Navbar"
 import { MainLayout } from "@/components/layout"
 import { Button } from "@/components/ui/button"
 import { NextPage } from "next"
 import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 import { PiWarningCircleFill } from "react-icons/pi"
 
+interface PatientData {
+  thaiId: string
+  password: string
+  firstname: string
+  lastname: string
+  age: number
+  gender: string
+  address: string
+  phoneNumber: string
+  healthRiskScore: number
+}
+
+//mock data
 const person1 = {
   firstName: "สมชาย",
   lastName: "ใจดี",
@@ -21,6 +34,47 @@ const person1 = {
 
 const personal: NextPage = () => {
   const router = useRouter()
+  const [patient, setPatient] = useState<PatientData>({} as PatientData)
+  const [medicalCondition, setMedicalCondition] = useState<string[]>([])
+
+  //value will be received from route later on
+  const thaiId = "111"
+
+  //fetch patients table by thaiId
+  useEffect(() => {
+    fetch(`http://localhost:4263/patients/${thaiId}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok")
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setPatient(data)
+        console.log("Patient data fetched successfully:", data)
+      })
+      .catch((error) => {
+        console.error("Error fetching patient data:", error)
+      })
+  }, [])
+
+  //fetch medical conditions by thaiId
+  useEffect(() => {
+    fetch(`http://localhost:4263/patients/${thaiId}/medicalconditions`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok")
+        }
+        return response.json()
+      })
+      .then((data) => {
+        setMedicalCondition(data)
+        console.log("medical conditions fetched successfully:", data)
+      })
+      .catch((error) => {
+        console.error("Error fetching patient data:", error)
+      })
+  }, [])
 
   const goToMainPage = () => {
     router.push("/foodLog")
@@ -37,22 +91,23 @@ const personal: NextPage = () => {
         <div className="flex flex-col gap-4 bg-white rounded-md p-5 text-text text-base">
           <div className="flex flex-col gap-2">
             <p className="font-bold">
-              ชื่อ: <span className="font-normal">{person1.firstName}</span>
+              ชื่อ: <span className="font-normal">{patient.firstname}</span>
             </p>
             <p className="font-bold">
-              นามสกุล: <span className="font-normal">{person1.lastName}</span>
+              นามสกุล: <span className="font-normal">{patient.lastname}</span>
             </p>
             <p className="font-bold">
-              อายุ: <span className="font-normal">{person1.age}</span>
+              อายุ: <span className="font-normal">{patient.age}</span>
             </p>
             <p className="font-bold">
-              เพศ: <span className="font-normal">{person1.gender}</span>
+              เพศ: <span className="font-normal">{patient.gender}</span>
             </p>
             <p className="font-bold">
-              ที่อยู่: <span className="font-normal">{person1.address}</span>
+              ที่อยู่: <span className="font-normal">{patient.address}</span>
             </p>
             <p className="font-bold">
-              เบอร์โทร: <span className="font-normal">{person1.tel}</span>
+              เบอร์โทร:{" "}
+              <span className="font-normal">{patient.phoneNumber}</span>
             </p>
           </div>
           <div className="h-auto">
@@ -61,7 +116,7 @@ const personal: NextPage = () => {
           <div className="text-[#C31936]">
             <p>โรคประจำตัว:</p>
             <ul className="px-2">
-              {person1.medicalCondition.map((condition, index) => (
+              {medicalCondition.map((condition, index) => (
                 <li key={index}>- {condition}</li>
               ))}
             </ul>
