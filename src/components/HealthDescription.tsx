114b53958ddc67fe8561432c8c26c2df411f35c0
NattawatC <64011478@kmitl.ac.th>
2024-04-08 01:24:02 +0700

    Add logic for HealthDescription

--- src/components/HealthDescription.tsx
+++ src/components/HealthDescription.tsx
@@ -6,24 +6,41 @@ interface HealthScore {
 
 const HealthDescription: React.FunctionComponent<HealthScore> = ({ score }) => {
   return (
-    <div className="bg-white rounded-md p-5 text-[#C31936] shadow-lg">
-      <p className="flex flex-row gap-1 items-center text-xl font-bold">
-        <PiWarningCircleFill size={24} />
-        ข้อควรระวัง
-      </p>
-      {/* <p>{person1.description}</p> */}
-      <p>{score}</p>
-      {score === 0 ? (
-        // Render when HealthScore is 0
-        <p>HealthScore is 0</p>
-      ) : score ? (
-        // Render when score is 1, 2, or 3
-        <p>score is 1, 2, or 3</p>
+    <>
+      {score === 1 ? (
+        <div className="bg-white rounded-md p-5 text-[#C9E3F1] shadow-lg">
+          <p className="flex flex-row gap-1 items-center text-xl font-bold">
+            <PiWarningCircleFill size={24} />
+            ข้อควรระวัง
+          </p>
+          <p>{score}</p>
+        </div>
+      ) : score === 2 ? (
+        <div className="bg-white rounded-md p-5 text-[#ED713D] shadow-lg">
+          <p className="flex flex-row gap-1 items-center text-xl font-bold">
+            <PiWarningCircleFill size={24} />
+            ข้อควรระวัง
+          </p>
+          <p>{score}</p>
+        </div>
+      ) : score === 3 ? (
+        <div className="bg-white rounded-md p-5 text-[#C31936] shadow-lg">
+          <p className="flex flex-row gap-1 items-center text-xl font-bold">
+            <PiWarningCircleFill size={24} />
+            ข้อควรระวัง
+          </p>
+          <p>{score}</p>
+        </div>
       ) : (
-        // Render for other cases
-        <p>score is neither 0 nor 1, 2, 3</p>
+        <div className="bg-white rounded-md p-5 text-text shadow-lg">
+          <p className="flex flex-row gap-1 items-center text-xl font-bold">
+            <PiWarningCircleFill size={24} />
+            ข้อควรระวัง
+          </p>
+          <p>{score}</p>
+        </div>
       )}
-    </div>
+    </>
   )
 }
 export default HealthDescription
