99e769a3cd90320e33788bf4bf252ca04b5b4f90
NattawatC <64011478@kmitl.ac.th>
2024-04-10 04:12:10 +0700

    Get patientId and its reviewStatus

--- src/pages/patient.tsx
+++ src/pages/patient.tsx
@@ -4,12 +4,7 @@ import { Navbar } from "@/components/Navbar"
 import { MainLayout } from "@/components/layout"
 import PatientCard from "@/components/patientCard"
 import { NextPage } from "next"
-import { useRouter } from "next/router"
 import { useEffect, useState } from "react"
-
-interface ThaiId {
-  thaiId: string
-}
 
 interface PatientData {
   status: string
@@ -21,24 +16,14 @@ interface PatientData {
   thaiId: string
 }
 
+interface PatientStatus {
+  patientId: string
+  reviewStatus: boolean
+}
+
 const Patient: NextPage = () => {
   const [patients, setPatients] = useState<PatientData[]>([])
-  const router = useRouter()
-
-  // useEffect(() => {
-  //   fetchPatients()
-  // }, [])
-
-  // const fetchPatients = async () => {
-  //   try {
-  //     const response = await fetch("http://localhost:4263/patients")
-  //     const data = await response.json()
-  //     setPatients(data)
-  //     console.log("patients fetched: ", data)
-  //   } catch (error) {
-  //     console.error("Error fetching patients:", error)
-  //   }
-  // }
+  const [status, setStatus] = useState<PatientStatus[]>([])
 
   useEffect(() => {
     const fetchPatientsAndMedicalConditions = async () => {
@@ -47,11 +32,10 @@ const Patient: NextPage = () => {
         if (!response.ok) {
           throw new Error("Failed to fetch patients")
         }
-        const patientsData = await response.json()
+        const patientsData: PatientData[] = await response.json()
 
         const patientsWithMedicalConditions = await Promise.all(
-          patientsData.map(async (patient: any) => {
-            // Add type any here
+          patientsData.map(async (patient: PatientData) => {
             const medicalConditionsResponse = await fetch(
               `http://localhost:4263/patients/${patient.thaiId}/medicalconditions`
             )
@@ -60,12 +44,11 @@ const Patient: NextPage = () => {
             }
             const medicalConditions = await medicalConditionsResponse.json()
             const medicalConditionString = medicalConditions.join(", ")
-            return { ...patient, medicalCondition: medicalConditionString } // Change property name to match PatientData
+            return { ...patient, medicalCondition: medicalConditionString }
           })
         )
 
         setPatients(patientsWithMedicalConditions)
-        console.log(patientsWithMedicalConditions)
       } catch (error) {
         console.error("Error fetching patients and medical conditions:", error)
       }
@@ -74,9 +57,49 @@ const Patient: NextPage = () => {
     fetchPatientsAndMedicalConditions()
   }, [])
 
-  // const goToAddFood = () => {
-  //   router.push("/addFood");
-  // };
+  useEffect(() => {
+    const fetchPatientsWithReviewStatus = async () => {
+      try {
+        const response = await fetch("http://localhost:4263/patients")
+        if (!response.ok) {
+          throw new Error("Failed to fetch patients")
+        }
+        const patientsData: PatientData[] = await response.json()
+
+        const mealsData = await Promise.all(
+          patientsData.map(async (patient: PatientData) => {
+            const mealsResponse = await fetch(
+              `http://localhost:4263/patients/${patient.thaiId}/meals`
+            )
+            if (!mealsResponse.ok) {
+              throw new Error("Failed to fetch meals")
+            }
+            const mealsData: PatientStatus[] = await mealsResponse.json()
+
+            const reviewStatuses = mealsData.map((meal) => meal.reviewStatus)
+
+            const status =
+              reviewStatuses.length > 0 &&
+              reviewStatuses.every(
+                (status) => status === true || status === undefined
+              )
+
+            const patientStatus: PatientStatus = {
+              patientId: patient.thaiId,
+              reviewStatus: status,
+            }
+
+            return patientStatus
+          })
+        )
+        setStatus(mealsData)
+      } catch (error) {
+        console.error("Error fetching patients and medical conditions:", error)
+      }
+    }
+
+    fetchPatientsWithReviewStatus()
+  }, [])
 
   return (
     <>
@@ -86,17 +109,25 @@ const Patient: NextPage = () => {
           <h1 className="flex justify-center text-3xl">รายชื่อผู้ใช้</h1>
         </div>
         <div className="flex flex-col gap-4">
-          {patients.map((person) => (
-            <PatientCard
-              key={person.thaiId}
-              status={person.status}
-              firstName={person.firstname}
-              lastName={person.lastname}
-              age={person.age}
-              gender={person.gender}
-              medicalCondition={person.medicalCondition}
-            />
-          ))}
+          {patients.map((person, item) => {
+            const patientStatus = status.find(
+              (s) => s.patientId === person.thaiId
+            )
+            console.log(patientStatus)
+
+            return (
+              <PatientCard
+                key={item}
+                patientId={person.thaiId}
+                status={patientStatus?.reviewStatus || false}
+                firstName={person.firstname}
+                lastName={person.lastname}
+                age={person.age}
+                gender={person.gender}
+                medicalCondition={person.medicalCondition}
+              />
+            )
+          })}
         </div>
       </MainLayout>
     </>
